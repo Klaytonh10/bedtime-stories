7e4dd4a2e80d146d3847c10651869d110ba884e9
Klaytoh10 <klaytonh10@gmail.com>
2025-10-09 08:51:37 -0500

    push

--- src/main/java/org/pluralsight/Main.java
+++ src/main/java/org/pluralsight/Main.java
@@ -1,7 +1,23 @@
 package org.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
 
+
+        try {
+            FileReader fileReader1 = new FileReader("src/main/resources/goldilocks.txt");
+            FileReader fileReader2 = new FileReader("src/main/resources/hansel_and_gretel.txt");
+            FileReader fileReader3 = new FileReader("src/main/resources/mary_had_a_little_lamb.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader1);
+
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
     }
 }
